252457a071f1e537104077db9d51df24f5437c32
Van Bui <van.bt.38@gmail.com>
2023-07-03 14:58:02 +0700

    remove code contact form du

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -224,37 +224,4 @@ jQuery(function ($) {
         }).init();
 
     }());
-
-    // -------------------------------------------------------------
-    // Contact Form
-    // -------------------------------------------------------------
-
-    $('#contactForm').on('submit',function(e){
-
-        e.preventDefault();
-
-        var $action = $(this).prop('action');
-        var $data = $(this).serialize();
-        var $this = $(this);
-
-        $this.prevAll('.alert').remove();
-
-        $.post( $action, $data, function( data ) {
-
-            if( data.response=='error' ){
-
-                $this.before( '<div class="alert alert-danger">'+data.message+'</div>' );
-            }
-
-            if( data.response=='success' ){
-
-                $this.before( '<div class="alert alert-success">'+data.message+'</div>' );
-                $this.find('input, textarea').val('');
-            }
-
-        }, "json");
-
-    });
-
-
 });
